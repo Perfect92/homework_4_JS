88822fc42617a631b242395a670d130816f4995d
Perfect92 <bakytbekovnurzanbek@gmail.com>
2022-11-15 20:33:18 +0300

    home work

---
+++ lesson_4.js
@@ -0,0 +1,16 @@
+// 1 Задача
+function button () {
+    alert(input.value)
+}
+// 2 Задача
+let months = 'Ноябрь'
+if (months === 'Март'  ) {
+     alert('Март')
+}else if (months === 'Сентябрь') {
+        alert('Сентябрь')
+}else {
+    alert('Ноябрь')
+}
+
+
+
